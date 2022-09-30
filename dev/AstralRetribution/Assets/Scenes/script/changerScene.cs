af85988fffe38d8d074cc9154fb21d5b1216c624
1569047 <e.wcaron@etu.cvm.qc.ca>
2022-09-30 15:33:42 -0400

    changemnt de scene

---
+++ dev/AstralRetribution/Assets/Scenes/script/changerScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changerScene : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("AstralRetribution_MenuInitial");
+    }
+
+    public void MenuOption()
+    {
+        SceneManager.LoadScene("AstralRetribution_MenuOption");
+    }
+}
