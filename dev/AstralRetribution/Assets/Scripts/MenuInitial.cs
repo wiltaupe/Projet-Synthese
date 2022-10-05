d0b1bc90aac0ccd647509ff340b3e35fe1d4ca16
DavidDemers08 <e.ddemers3@etu.cvm.qc.ca>
2022-10-05 18:47:48 -0400

    enfin c'est easy dude

---
+++ dev/AstralRetribution/Assets/Scripts/MenuInitial.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInitial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DemarrerPartie()
+    {
+
+    }
+
+    public void QuitterPartie()
+    {
+        Application.Quit();
+    }
+    public void Options()
+    {
+        SceneManager.LoadSceneAsync("AstralRetribution_MenuOption");
+    }
+}
---
+++ dev/AstralRetribution/Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Quitter()
+    {
+        SceneManager.LoadSceneAsync("AstralRetribution_MenuInitial");
+    }
+}
