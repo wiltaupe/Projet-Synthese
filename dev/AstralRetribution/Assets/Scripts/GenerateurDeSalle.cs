dd3ac041f990a4958a97db8976909ea29af2345f
1942878 <e.ddemers3@etu.cvm.qc.ca>
2022-10-07 14:24:58 -0400

    allo

--- dev/AstralRetribution/Assets/Scripts/GenerateurDeSalle.cs
+++ dev/AstralRetribution/Assets/Scripts/GenerateurDeSalle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateurDeSalle
@@ -6,13 +7,14 @@ public class GenerateurDeSalle
     public ArbreBinaire arbreBinaire;
     public int nbSalleVoulue;
     public Vector2 taille;
-    private Vector2[] points;
+    private List<Vector2> listePoints;
 
     public GenerateurDeSalle(int nbSalleVoulue, Vector2 taille)
     {
         this.nbSalleVoulue = nbSalleVoulue;
         this.taille = taille;
         arbreBinaire = new ArbreBinaire();
+        listePoints = new List<Vector2>();
         GenererPoints();
     }
 
@@ -22,12 +24,19 @@ public class GenerateurDeSalle
         {
             for (int j = 0; j < taille.y; j++)
             {
-                
+                Vector2 vector2 = new Vector2(i, j);
+                Debug.Log(vector2);
+                listePoints.Add(vector2);
             }
         }
     }
 
     public void Division(NoeudArbre noeud,float pourcentage,bool noeudFait)
+    {
+
+    }
+
+    public void AjouterPoints(Vector2[] vector2s)
     {
 
     }
