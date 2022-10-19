d689afb51655d2ff611bae0f2411046795d84e4b
1569047 <e.wcaron@etu.cvm.qc.ca>
2022-10-19 17:17:24 -0400

    pas capable de toucher a la grille que david a fournis comme lui

--- dev/AstralRetribution/Assets/Scripts/MenuHub.cs
+++ dev/AstralRetribution/Assets/Scripts/MenuHub.cs
@@ -5,16 +5,37 @@ public class menuhub : MonoBehaviour
 {
     public TileBase gridtile;
 
-    GameObject grid;
-    Tilemap vaisseau;
+    GameObject grid,gridtest;
+    Tilemap vaisseau,plancher,mur;
 
     // Start is called before the first frame update
     void Start()
     {
-        grid = GameObject.Find("Vaisseau_toit");
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Grid");
+        plancher = objs[0].GetComponent<Tilemap>();
+        mur = objs[1].GetComponent<Tilemap>();
+
+        //objs[0].transform.position = new Vector3(-398,-40,1);
+
+        //t_plan.Scale.X = 0.75;
+        //t_plan.Scale.Y = 0.75;
+
+        //tilemap.SetTileFlags(cellPos, TileFlags.None);
+        //tilemap.SetTileFlags(position, TileFlags.None);
+        //plancher.SetColor(position, Color.blue);
+        //gridtest = GameObject.Find("GameZone");
+
+        //Debug.Log(gridtest);
+        //gridtest.SetActive(false);
+
+
+        /*grid = GameObject.Find("Vaisseau_toit");
         vaisseau = grid.GetComponent<Tilemap>();
 
         vaisseau.SetTile(new Vector3Int(1, 0, 0), gridtile);
+        vaisseau.SetTile(new Vector3Int(2, 0, 0), gridtile);
+        vaisseau.SetTile(new Vector3Int(2, 1, 0), gridtile);
+        vaisseau.SetTile(new Vector3Int(2, 2, 0), gridtile);*/
     }
 
     // Update is called once per frame
