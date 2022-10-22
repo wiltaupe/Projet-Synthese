0e2f86314b4e41ea201552326d3e696d9c4c487d
DavidDemers08 <e.ddemers3@etu.cvm.qc.ca>
2022-10-22 18:24:29 -0400

    drag and drop

---
+++ dev/AstralRetribution/Assets/DragDrop.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private Vector3 dragOffset;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        dragOffset = transform.position - GetMousePos();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMousePos() + dragOffset;
+    }
+
+    Vector3 GetMousePos()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+
+    private void OnMouseUp()
+    {
+        Collider2D col = Physics2D.OverlapPoint(GetMousePos(),LayerMask.GetMask("Sol"));
+        this.transform.position = col.gameObject.transform.position;
+    }
+}
